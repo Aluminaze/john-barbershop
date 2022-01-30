2ac7ca931bac75b1b0919295f4bb210d3893ad8d
Aluminaze <aluminaze@gmail.com>
2022-01-30 18:20:17 +0200

    add new icons

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,6 +1,8 @@
+import { Instagram } from "./Instagram/index";
 import { Barber } from "./Barber";
 import { Location } from "./Location";
 import { Time } from "./Time";
 import { Calendar } from "./Calendar";
+import { Whatsapp } from "./Whatsapp";
 
-export const Icons = { Barber, Location, Time, Calendar };
+export const Icons = { Barber, Location, Time, Calendar, Whatsapp, Instagram };
