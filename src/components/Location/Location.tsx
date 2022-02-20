ae45dbb3414df3aa262e2f128f9a6a78b3e3492c
Aluminaze <aluminaze@gmail.com>
2022-02-20 14:19:14 +0200

    add link to 2gis

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -2,6 +2,8 @@ import Typography from "@mui/material/Typography";
 import { Icons } from "components/Icons";
 import { useStyles } from "./styles";
 
+import { config } from "config";
+
 export const Location = () => {
   const classes = useStyles();
 
@@ -10,26 +12,29 @@ export const Location = () => {
       <Typography variant="play_bold" sx={{ fontSize: "1.4rem" }}>
         Мы находимся и работаем
       </Typography>
-      <div className={classes.items}>
-        <div className={classes.item}>
-          <Icons.Location sx={{ fontSize: "30px" }} />
-          <Typography variant="play_regular" className={classes.itemText}>
-            г. Калининград, ул. Багратиона, 43-47
-          </Typography>
+
+      <a href={config["2gis"]} target="_blank" rel="noreferrer">
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <Icons.Location sx={{ fontSize: "30px" }} />
+            <Typography variant="play_regular" className={classes.itemText}>
+              г. Калининград, ул. Багратиона, 43-47
+            </Typography>
+          </div>
+          <div className={classes.item}>
+            <Icons.Calendar sx={{ fontSize: "30px" }} />
+            <Typography variant="play_regular" className={classes.itemText}>
+              7 дней в неделю
+            </Typography>
+          </div>
+          <div className={classes.item}>
+            <Icons.Time sx={{ fontSize: "30px" }} />
+            <Typography variant="play_regular" className={classes.itemText}>
+              10:00 - 21:00
+            </Typography>
+          </div>
         </div>
-        <div className={classes.item}>
-          <Icons.Calendar sx={{ fontSize: "30px" }} />
-          <Typography variant="play_regular" className={classes.itemText}>
-            7 дней в неделю
-          </Typography>
-        </div>
-        <div className={classes.item}>
-          <Icons.Time sx={{ fontSize: "30px" }} />
-          <Typography variant="play_regular" className={classes.itemText}>
-            10:00 - 21:00
-          </Typography>
-        </div>
-      </div>
+      </a>
     </div>
   );
 };
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  "2gis": "https://go.2gis.com/jfpv4c",
+};
